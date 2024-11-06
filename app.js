e915503b232f36aa97a703faf1c13d999d663809
Hayyuu <hayatahimed518@gmail.com>
2024-11-06 12:37:10 +0300

    Add form to send a new message and a page to view details of each message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const path=require('node:path');
 const app=express();
+app.use(express.urlencoded({ extended: true }));
 console.log("hii");
 
 app.set("views",path.join(__dirname,"Views"));
@@ -20,4 +21,13 @@ let message=[
 app.listen(3000,()=>console.log("started..."));
 app.get('/',(req,res)=>{
     res.render("index",{ title: "Message Board",messages:message});
+});
+app.get('/new',(req,res)=>res.render('form'));
+app.post('/new',(req,res)=>{
+    message[message.length]={user:req.body.user,text:req.body.text,added:new Date()};
+    res.render('index',{title:"Updated Message Board",messages:message})
+});
+app.get('/:index',(req,res)=>{
+    console.log(req.params);
+    res.render('detail',{message:message[req.params.index]})
 });
